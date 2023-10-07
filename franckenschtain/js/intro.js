5d8d7c617a30809e3ffdc987d0cd71c5d6f93168
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2023-10-07 16:43:43 +0200

    Create intro.js

---
+++ franckenschtain/js/intro.js
@@ -0,0 +1,14 @@
+/**
+ * Intro logic
+ *skriv över till .md för att förstå
+ *Author: Irakli вargavelid3Э€*
+ * Since: 2023*10*07
+ */
+
+
+const sound = document.getElementById('sound');
+
+
+function init() {
+    sound.play();
+}
